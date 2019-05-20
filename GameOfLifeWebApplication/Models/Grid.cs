936feb1a9144392a458de9c8f7f6e30090743868
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-20 13:50:17 -0400

    Changed the model to a bool 2d array and got the view to reflect changes from the model

--- GameOfLifeWebApplication/Models/Grid.cs
+++ GameOfLifeWebApplication/Models/Grid.cs
@@ -7,18 +7,18 @@ namespace GameOfLifeWebApplication.Models
 {
     public class Grid
     {
-        public int[,] grid;
+        public bool[,] grid;
         public int generation = 0;
 
         public Grid(int rows, int columns)
         {
-            this.grid = new int[rows, columns];
+            this.grid = new bool[rows, columns];
 
             for(int i = 0; i < rows; i++)
             {
                 for(int j = 0; j < columns; j++)
                 {
-                    this.grid[i, j] = 0;
+                    this.grid[i, j] = true;
                 }
             }
         }
